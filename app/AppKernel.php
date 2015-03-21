4c6ad57690aa937332d94c156dec9692df7a6a23
Mads Røskar <madshvero@gmail.com>
2015-03-21 20:56:55 +0100

    Adds the frontend bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new Kamikaze3\Bundle\WriterFrontendBundle\Kamikaze3WriterFrontendBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/Kamikaze3/Bundle/WriterFrontendBundle/Kamikaze3WriterFrontendBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Kamikaze3\Bundle\WriterFrontendBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class Kamikaze3WriterFrontendBundle extends Bundle
+{
+}
